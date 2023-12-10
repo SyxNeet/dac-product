a5b7460f65dec3d3cdf5032ccd7656f8b65910d4
sonnt <ngotheson1@gmail.com>
2023-12-10 16:14:52 +0700

    update gsap reload

--- src/components/common/GsapSmoother.js
+++ src/components/common/GsapSmoother.js
@@ -9,17 +9,25 @@ import { useMediaQuery } from "react-responsive";
 export default function GsapSmoother({ children }) {
   const isMobile = useMediaQuery({ query: "(max-width: 767.9px)" });
   useEffect(() => {
-    
-    if(!isMobile){
-      gsap.registerPlugin(ScrollSmoother);
-
-      ScrollSmoother.create({
-        smooth: 0.8,
-        effects: true,
-        smoothTouch: 0.1,
-      });
+    try {
+      if (!isMobile) {
+        gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+        ScrollSmoother.create({
+          smooth: 0.8,
+          effects: true,
+          smoothTouch: 0.1,
+        });
+  
+        return () => {
+          gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+        };
+      }
+    } catch (error) {
+      console.error('Error in GSAP initialization:', error);
     }
-  });
+  }, []);
+  
+  
   return (
     <div id="smooth-wrapper">
       <div id="smooth-content">{children}</div>
