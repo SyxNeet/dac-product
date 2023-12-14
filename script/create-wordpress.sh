c163a21bcbe592f74876a9488699414606eb2498
sonnt <sonnt@toprate.io>
2023-12-14 18:33:53 +0700

    install nginx cerbot

---
+++ script/create-wordpress.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Update the apt package index
+sudo apt update
+
+# Install Nginx
+sudo apt install -y nginx
+
+# Allow Nginx through the firewall
+sudo ufw allow 'Nginx Full'
+
+# Reload the firewall
+sudo ufw reload
+
+# Test Nginx configuration
+sudo nginx -t
+
+# If the test is successful, restart Nginx
+sudo systemctl restart nginx
+
+# Install MySQL Server
+sudo apt install -y mysql-server
+
+# Secure MySQL installation
+sudo mysql_secure_installation
+
+# Install PHP and required extensions
+sudo apt install -y php-fpm php-mysql
+
+# Configure Nginx for the domain cms.appjsc.com
+sudo tee /etc/nginx/sites-available/cms.appjsc.com <<EOF
+server {
+    listen 80;
+    server_name cms.appjsc.com www.cms.appjsc.com;
+
+    root /var/www/html/cms.appjsc.com;
+    index index.php index.html index.htm;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$args;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOF
+
+# Create site enable
+sudo ln -s /etc/nginx/sites-available/cms.appjsc.com /etc/nginx/sites-enabled/
+
+# Create the document root directory
+sudo mkdir -p /var/www/html/cms.appjsc.com
+
+# Set permissions
+sudo chown -R www-data:www-data /var/www/html/cms.appjsc.com
+
+# Download and extract WordPress
+sudo curl -O https://wordpress.org/latest.tar.gz
+sudo tar xf latest.tar.gz
+sudo cp -r wordpress/* /var/www/html/cms.appjsc.com/
+
+# Configure WordPress
+sudo cp /var/www/html/cms.appjsc.com/wp-config-sample.php /var/www/html/cms.appjsc.com/wp-config.php
+sudo chown www-data:www-data /var/www/html/cms.appjsc.com/wp-config.php
+
+# Generate secret keys for WordPress
+sudo curl -s https://api.wordpress.org/secret-key/1.1/salt/ > /tmp/wp-salt
+sudo sed -i '/AUTH_KEY/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo sed -i '/SECURE_AUTH_KEY/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo sed -i '/LOGGED_IN_KEY/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo sed -i '/NONCE_KEY/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo sed -i '/AUTH_SALT/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo sed -i '/SECURE_AUTH_SALT/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo sed -i '/LOGGED_IN_SALT/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo sed -i '/NONCE_SALT/d' /var/www/html/cms.appjsc.com/wp-config.php
+sudo cat /tmp/wp-salt >> /var/www/html/cms.appjsc.com/wp-config.php
+
+# Create MySQL database and user
+DB_NAME="cms_appjsc_com"
+DB_USER="cms_user"
+DB_PASSWORD=$(openssl rand -base64 12)
+
+# Create the database
+sudo mysql -e "CREATE DATABASE $DB_NAME;"
+
+# Create the database user and grant privileges
+sudo mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+
+# Test Nginx configuration again
+sudo nginx -t
+
+# If the test is successful, restart Nginx
+sudo systemctl restart nginx
+
+# Display a message indicating successful installation
+echo "LEMP stack (Nginx, MySQL, PHP), WordPress, and MySQL database have been successfully installed for the domain cms.appjsc.com."
+echo "MySQL Database: $DB_NAME"
+echo "MySQL User: $DB_USER"
+echo "MySQL Password: $DB_PASSWORD"
