0653da6c13ac092db6ca2ac32f3232b2c3b77a78
SyxNeet <110085910+SyxNeet@users.noreply.github.com>
2023-12-28 07:02:06 -0800

    redirect home mb

--- src/components/lang/SelectLangMb.js
+++ src/components/lang/SelectLangMb.js
@@ -30,7 +30,8 @@ function SelectLangMb({ lang, handleCloseModal }) {
                     // }}
                     key={index}
                     // href={redirectedPathName(locale.locale)}
-                    href={locale.locale==='vi'?slug?.slugVi||'/':slug?.slugEn||'/en'}
+                    // href={locale.locale==='vi'?slug?.slugVi||'/':slug?.slugEn||'/en'}
+                    href={locale.locale==='vi'? '/': '/en'}
                     className={`flex link items-center w-full  border-solid  px-[0.6rem] ${index === 0 ? 'border-b border-solid pb-[1rem] border-[#000]' : ''} ${lng === locale.locale ? 'active' : ''}`}
                 >
                     <span className={`uppercase text-[3.2rem] text-black`}>{locale.locale}</span>
